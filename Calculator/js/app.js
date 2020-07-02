5f286e24b501b2fc80ef42eec6de1fe7585efe19
Mohamed Saleem <67532838+SaleemIlahi@users.noreply.github.com>
2020-07-02 14:33:25 +0530

    Add files via upload

---
+++ Calculator/js/app.js
@@ -0,0 +1,59 @@
+
+//getting history value
+function gethistory(){
+    return document.getElementById("history-value").innerHTML;
+}
+function printhistory(num){
+    document.getElementById("history-value").innerHTML = num;
+}
+
+//getting output value
+function getoutput(){
+    return document.getElementById("result").innerText;
+}
+function printoutput(num){
+    document.getElementById("result").innerText = num;
+}
+
+//operation
+var operator = document.getElementsByClassName("operator");
+for(var i = 0; i < operator.length ; i++){
+    operator[i].addEventListener("click",function(){
+        if(this.id == "clear"){
+            printhistory("");
+            printoutput("");
+        }
+        else if(this.id == "backspace"){
+            var output = getoutput();
+            output = output.substr(0,output.length-1);
+            printoutput(output);
+        }
+        else{
+            var history = gethistory();
+            var output = getoutput();
+            if(output != ""){
+                history = history + output;
+            }
+            if(this.id == "="){
+                var result = eval(history);
+                printoutput(result);
+                printhistory("");
+            }
+            else{
+                history = history + this.id;
+                printhistory(history);
+                printoutput("");
+            }
+        }   
+    });
+}
+
+//number
+var number = document.getElementsByClassName("number");
+for(var i = 0; i < number.length ; i++){
+    number[i].addEventListener("click",function(){
+        var output = getoutput();
+        output = output + this.id;
+        printoutput(output);
+    });
+}
